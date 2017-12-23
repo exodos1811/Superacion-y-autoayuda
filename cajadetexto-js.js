4188c3be13db2c4f6170a9f2c477f068a26ef7f8
exodos1811 <34783907+exodos1811@users.noreply.github.com>
2017-12-23 06:41:02 +0330

    Add files via upload

---
+++ cajadetexto-js.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+       $(".bt").click(function(){
+           
+           var com = $("#com").val();
+           if (com != ""){
+               alert("send messenge ");
+               
+           }else{
+               alert("not messenge");
+           }
+       });
+});
+
+
+
+
